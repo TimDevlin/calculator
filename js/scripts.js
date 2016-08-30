c261fc1da9ae8453b856f4cb2c2bd515e3d74a82
caitlin ashtari and tim devlin <caitlin@>
2016-08-29 20:40:53 -0700

    revise js code

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ var add = function(number1, number2) {
 return number1 + number2;
 };
 
-var number1 = prompt(parseInt("Enter a number"));
+var number1 = parseInt(prompt("Enter a number"));
 
-var number2 = prompt(parseInt("Enter another number"));
+var number2 = parseInt(prompt("Enter another number"));
 
 alert(add(number1, number2));
