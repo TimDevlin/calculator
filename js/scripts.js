29f2c9a888a18085a94fa1e9f22c351f60df06c3
caitlin ashtari and tim devlin <caitlin@>
2016-08-29 20:34:56 -0700

    create calculator repository with js, css, html.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = prompt(parseInt("Enter a number"));
+
+var number2 = prompt(parseInt("Enter another number"));
+
+alert(add(number1, number2));
